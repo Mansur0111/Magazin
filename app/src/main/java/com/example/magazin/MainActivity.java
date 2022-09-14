2b2fc21f2e4cbe61361b841e22e1e21600f738c9
User <abduraximovjasur125@gmail.com>
2022-09-14 17:56:55 +0500

    tekshiring

---
+++ app/src/main/java/com/example/magazin/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.magazin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+ImageView imageView;
+DrawerLayout drawerLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView=findViewById(R.id.imageviewmenu);
+        drawerLayout=findViewById(R.id.drawerlayout);
+        imageView.setOnClickListener(view -> {
+            drawerLayout.openDrawer(Gravity.LEFT);
+        });
+    }
+}
